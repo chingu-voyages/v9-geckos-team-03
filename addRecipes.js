41097e5825e9e132760aa9cdac3e6e067db9887c
Debjani Paul <debjanideb@hotmail.com>
2019-06-21 21:40:29 -0700

    Added Search feature and Delete button functionality

--- addRecipes.js
+++ addRecipes.js
@@ -1,4 +1,5 @@
 let mainBtn = document.getElementById('mainBtn');
+let backBtn = document.getElementById('backBtn');
 let recipeName = document.getElementById('recipeName');
 let imageUrl = document.getElementById('imageUrl');
 let providerName = document.getElementById('providerName');
@@ -8,6 +9,10 @@ let directions = document.getElementById('directions');
 let submitBtn = document.getElementById('submitBtn');
 let formContainer = document.getElementById('formContainer');
 let closeBtn = document.getElementById('closeBtn');
+let ul = document.getElementById("recipeContainer");
+let li = ul.getElementsByTagName('li');
+let deleteCard = document.getElementById('deleteCard');
+let lastid = 0;
 
 
 //Function to toggle the 'Add Recipes' button to show and hide the 'Add recipe' form
@@ -43,8 +48,10 @@ function create_cards() {
   let ingredientsValue = ingredients.value;
   let directionsValue = directions.value;
   let recipeContainer =document.getElementById('recipeContainer')
-  let recipeCardContent = document.createElement('div');
+  let recipeCardContent = document.createElement('li');
       recipeCardContent.classList.add('recipeCards');
+      recipeCardContent.setAttribute('id','item'+lastid);
+      // lastid+=1;
       recipeContainer.appendChild(recipeCardContent);
 
   let imgContainer = document.createElement('div');
@@ -80,12 +87,22 @@ function create_cards() {
       aFirstTag.innerHTML = "Details";
       buttonContainer.appendChild(aFirstTag);
 
-  let aSecTag = document.createElement('a');
-      aSecTag.setAttribute('href', sourceUrlValue);
-      aSecTag.setAttribute('class', 'recipeUrl');
-      aSecTag.setAttribute('target', '_blank');
-      aSecTag.innerHTML = "Recipe URL";
-      buttonContainer.appendChild(aSecTag);
+  let editDiv = document.createElement('Div');
+      editDiv.setAttribute('href', '');
+      editDiv.classList.add('fas', 'fa-pencil-alt', 'edit');
+      editDiv.setAttribute('onClick','editCard("'+'item'+lastid+'")');
+      editDiv.setAttribute('target', '');
+      buttonContainer.appendChild(editDiv);
+
+  let deleteDiv = document.createElement('Div');
+      deleteDiv.setAttribute('href', '');
+      // deleteDiv.setAttribute('id', 'deleteCard');
+      deleteDiv.setAttribute('onClick','removeCard("'+'item'+lastid+'")');
+      deleteDiv.classList.add('fas', 'fa-trash-alt', 'delete');
+      deleteDiv.setAttribute('target', '');
+      buttonContainer.appendChild(deleteDiv);
+
+      lastid+=1;
 }
 
 //function to reset the form input values
@@ -119,4 +136,65 @@ function validateForm() {
     return false;
   }
   return true;
+}
+
+//function to add click event to search button
+function clickSearch() {
+  searchRecipes();
+  if(searchInput.value === ""){
+    backBtn.style.display = 'none';
+    mainBtn.style.display = 'block';
+  }else{
+    backBtn.style.display = 'block';
+    mainBtn.style.display = 'none';
+  }
+
+  return false;
+}
+
+//function to add/filter the search List
+function searchRecipes() {
+  // Declare variables
+  let input, filter, ul, li, h3, i, txtValue;
+  input = document.getElementById('searchInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("recipeContainer");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    h3 = li[i].getElementsByTagName("h3")[0];
+    txtValue = h3.textContent || h3.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+  return false;
+}
+
+//Add a click eventListener to back button
+backBtn.addEventListener('click', function() {
+  let ul = document.getElementById("recipeContainer");
+  let li = ul.getElementsByTagName('li');
+  for (i = 0; i < li.length; i++) {
+    li[i].style.display = "";
+  }
+  backBtn.style.display = 'none';
+  mainBtn.style.display = 'block';
+  searchInput.value = "";
+})
+
+//Click on deleteIcon to delete a recipe card
+function removeCard(itemid){
+  //console.log('clicked');
+  const lis = [...document.querySelectorAll('#recipeContainer li')];
+  let item = document.getElementById(itemid);
+
+    for (const li of lis) {
+      li.addEventListener('click', function() {
+        li.parentNode.removeChild(item);
+      })
+    }
 }
